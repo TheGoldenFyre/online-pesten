7c549b83be7ac979616858aa33e3652c7ae88de3
Thomas van der Plas <t.d.vanderplas@students.uu.nl>
2020-10-17 14:21:14 +0200

    .

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ let io = require("socket.io")(http)
 
 const PORT = 80
 
+let games = {}
+const maxPlayers = 4
+
 app.get("/", (req, res) => {
     res.send("lmao")
 })
@@ -18,9 +21,9 @@ app.get("/lobby/:lobbyID", (req, res) => {
 io.on('connection', (socket) => {
     socket.on("connection-request", (lobbyID) => {
         console.log(`LobbyId: ${lobbyID}`)
-        if (lobbyID == 1) {
+        if (games[`${lobbyID}`] != null && io.sockets.adapter.rooms[`${lobbyID}`].length < maxPlayers) {
             socket.join(`${lobbyID}`)
-            socket.emit("connection-accept")
+            socket.emit("connection-accept", lobbyID)
         } else {
             socket.emit("connection-deny")
         }
