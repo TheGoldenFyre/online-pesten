6f547d8cfa799d3169ac2e066bfa97776485374a
Thomas van der Plas <t.d.vanderplas@students.uu.nl>
2020-10-17 13:33:21 +0200

    .

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 let path = require("path")
 let express = require("express")
 let app = express()
+let http = require("http").createServer(app)
+let io = require("socket.io")(http)
 
 const PORT = 80
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("./index.html"))
+    res.send("lmao")
 })
 
 app.get("/lobby/:lobbyID", (req, res) => {
     console.log(`Client trying to connect to lobby: ${req.params.lobbyID}`)
-    res.send("lmao")
+    res.sendFile(path.resolve("./index.html"))
 })
 
-app.listen(PORT, () => console.log(`Started running on port ${PORT}`))
+io.on('connection', (socket) => {
+    socket.on("connection-request", (lobbyID) => {
+        console.log(`LobbyId: ${lobbyID}`)
+        if (lobbyID == 1) {
+            socket.join(`${lobbyID}`)
+            socket.emit("connection-accept")
+        } else {
+            socket.emit("connection-deny")
+        }
+    })
+
+    socket.on("make-move", (lobbyID) => {
+        io.to(lobbyID).emit("aaa")
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Byebye")
+    })
+})
+
+http.listen(PORT, () => console.log(`Started running on port ${PORT}`))
